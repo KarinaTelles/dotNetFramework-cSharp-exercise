91159597025270d65fba0c46740999496c1a98dc
KarinaTelles <karinac.telles@gmail.com>
2020-12-01 10:01:16 -0300

    adicionando o read line

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace dotNetFrameworkCsharpExercise
         static void Main(string[] args)
         {
             Console.Write("Hello World");
+            Console.ReadLine();
 
         }
     }
